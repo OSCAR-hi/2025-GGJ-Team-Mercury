94a9940360d92d02a767b1423da4c581100b9bb0
Rouming <2689155733@qq.com>
2025-01-18 00:41:00 +0800

    删除部分库

--- Assets/RenderingOrderHighlighterTool/TK2DUI_Highlighter/Editor/TK2dHighlighterConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class TK2dHighlighterConstants : MonoBehaviour
-{
-    public const int NUMBER_OF_CLUSTERS = 5;
-    public const string TK2D_ICON = "TK2D_Icon";
-    public const string TK2D_LAYER = "TK2D_Layer";
-}
